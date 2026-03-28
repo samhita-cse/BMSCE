491b854ae8fb1223d54ef05f26621d859532999f
samhita-cse <samhitasrihari00@gmail.com>
2026-03-28 08:58:05 +0530

    Delete client directory

--- client/src/components/MetricCard.jsx
+++
@@ -1,9 +0,0 @@
-export function MetricCard({ label, value, helper }) {
-  return (
-    <article className="metric-card">
-      <p>{label}</p>
-      <h3>{value}</h3>
-      <span>{helper}</span>
-    </article>
-  );
-}
